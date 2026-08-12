8d1e83d89105f559f1691d422fc94cb134e85f10
caduzol <>
2026-08-12 13:31:35 -0300

    java script
    introdução

---
+++ introducao-javascript/01-doces/scripit.js
@@ -0,0 +1,13 @@
+//comentarios em javaScripit usamos duas barras
+//Criar variaveis
+//as variaveis guardam um valor de um tipo diferente
+
+ const nomeDoce = "bolo";
+ const preco = 40.00;
+ const disponiveis = true;
+
+
+console.log(nomeDoce, preco, disponiveis);
+document.getElementById(resultado).textContent
+`$(nomeDoce) custa R$ $(preco,tofixed(2)) - disponivel: ${disponivel}`;
+ 
